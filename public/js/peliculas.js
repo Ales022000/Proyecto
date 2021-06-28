f97ab2ac3ab5fbe8fd85cbb350825e4f8de47557
Isabela Moreno Chavarría <caylor1989@gmail.com>
2021-06-28 16:14:55 -0600

    creacion metodo obtener pelicula
    Este metodo no funciona porque en el video el jala la informacion de un archivo tipo json y hace la solicitud sin embargo aca no me carga un archivo tipo json cuando lo intento igual que el video

--- public/js/peliculas.js
+++ public/js/peliculas.js
@@ -2,4 +2,12 @@ const api_key = "api_key=4ed5aae0e5dd89775521a8ce7db46e80";
 const base_url = "https://www.themoviedb.org/";
 const api_url = base_url + '/discover/movie?sort_by=popularity.desc' + api_key;
 
+obtenerPelicula(api_url);
 
+function obtenerPelicula(url) {
+
+    fetch(url).then(res => res.json()).then(data => {
+
+      console.log(data);
+    })
+}
