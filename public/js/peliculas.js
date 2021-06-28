71194b8dfcdc9944c38b4aaa2b698da80aac5aa4
Isabela Moreno Chavarría <caylor1989@gmail.com>
2021-06-28 14:53:10 -0600

    creacion del javascript
    cree el javascript con el nombre de peliculas para hacer la conexion con la api

---
+++ public/js/peliculas.js
@@ -0,0 +1,3 @@
+const api_key = "api_key=4ed5aae0e5dd89775521a8ce7db46e80";
+const base_url = "https://www.themoviedb.org/";
+const api_url = base_url + '/discover/movie?sort_by=popularity.desc' + api_key;
