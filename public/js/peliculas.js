0461522f529f00dd8e287fd48a88032683dff8a4
Isabela Moreno Chavarría <caylor1989@gmail.com>
2021-06-28 16:20:41 -0600

    editar los links

--- public/js/peliculas.js
+++ public/js/peliculas.js
@@ -1,6 +1,6 @@
 const api_key = "api_key=4ed5aae0e5dd89775521a8ce7db46e80";
-const base_url = "https://www.themoviedb.org/";
-const api_url = base_url + '/discover/movie?sort_by=popularity.desc' + api_key;
+const base_url = "https://api.themoviedb.org/3";
+const api_url = base_url + '/discover/movie?sort_by=popularity.desc&' + api_key;
 
 obtenerPelicula(api_url);
 
