96a55dd99e3600545a6f3300ae4e01880f98668d
Isabela Moreno Chavarría <caylor1989@gmail.com>
2021-07-17 22:59:51 -0600

    creacion de variables y metodos
    Cree la constante de la url para busqueda de pelicula por id, tambien el metodo de mostrar peliculas de accion y cambie el nombre del metodo mostrar peliculas por mostrar peliculas populares.

--- public/js/peliculas.js
+++ public/js/peliculas.js
@@ -3,20 +3,21 @@ const base_url = "https://api.themoviedb.org/3";
 const api_url = base_url + '/discover/movie?sort_by=popularity.desc&' + api_key + '&language=es-ES';
 const image_url = 'https://image.tmdb.org/t/p/w500';
 const main = document.getElementById('main');
+const url_busqueda_PorId = "https://api.themoviedb.org/3/movie/497698?api_key=4ed5aae0e5dd89775521a8ce7db46e80&language=es-ES";
 
 
-obtenerPelicula(api_url);
+obtenerPeliculas(api_url);
 
-function obtenerPelicula(url) {
+function obtenerPeliculas(url) {
 
     fetch(url).then(res => res.json()).then(data => {
 
       console.log(data.results);
-      mostrarPelicula(data.results);
+      mostrarPeliculasPopulares(data.results);
     })
 }
 
-function mostrarPelicula(data) {
+function mostrarPeliculasPopulares(data) {
 
   main.innerHTML = '';
 
@@ -27,7 +28,7 @@ function mostrarPelicula(data) {
        pelicula.setAttribute('href', `/movie-info.html?id=${id}`)
        pelicula.classList.add('contenedorPeliculas');
        
-      
+       obtenerPeliculaPorId();
       
        pelicula.innerHTML = `
        
@@ -70,3 +71,71 @@ function mostrarPelicula(data) {
        main.appendChild(pelicula);
     });
 }
+
+function mostrarPeliculasDeAccion(data) {
+
+  main.innerHTML = '';
+
+    data.forEach(datosDeLaPelicula => {
+       
+       const {id, title, poster_path, vote_average, overview, runtime} = datosDeLaPelicula;
+       const pelicula = document.createElement('div');
+       pelicula.setAttribute('href', `/movie-info.html?id=${id}`)
+       pelicula.classList.add('contenedorPeliculas');
+       
+       obtenerPeliculaPorId(497698);
+      
+       pelicula.innerHTML = `
+       
+       <div class="contenedorTrending">
+       <img class="rounded-xl" src="${image_url + poster_path}" alt="${title}">
+       <div class="play">
+         <img src="./img/boton-de-play (3).svg" alt="Play">
+       </div>
+       <div class="contenedorCalificacion">
+         <h1 class="textoCalificacion">${vote_average}</h1>
+       </div>
+     </div>
+
+     <div class="contenedorInformacion ">
+       <h1 class="nombrePelicula">${title}</h1>
+
+       <h2 class="categoria">categoria:  ${id}</h2>
+
+       <div class="contenedorDuracion">
+         <h3 class="textoDuracion">${runtime}</h3>
+       </div>
+
+       <div class="textoInformacion overflow-hidden">
+         <p> ${overview} </p>
+       </div>
+       
+       <div class="favorito">
+       <img src="./img/estrella.svg" alt="Favorito">
+     </div>
+     <div class="descargar">
+       <a href="#">
+         <img src="./img/descargar.svg" alt="Descargar"></a>
+     </div>
+
+
+     </div>
+
+       `
+
+       main.appendChild(pelicula);
+    });
+}
+
+function obtenerPeliculaPorId() {
+
+
+ fetch(url_busqueda_PorId).then(res => res.json()).then(data => {
+
+  console.log(data.results);
+ 
+
+   })
+ }
+
+ 
