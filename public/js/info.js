5368833417326571ae6e0801b5ec174500035996
Ales022000 <alessandrocas2000@gmail.com>
2021-07-20 09:08:51 -0600

    Detalles de Pelicula

--- public/js/info.js
+++ public/js/info.js
@@ -1,29 +1,34 @@
-
-document.addEventListener('DOMContentLoaded', function () {
-    const api_key = "api_key=4ed5aae0e5dd89775521a8ce7db46e80";
-    const base_url = "https://api.themoviedb.org/3";
-    
-    
-    const queryString = window.location.search;
-    console.log(queryString);
-    const id = queryString.substring( queryString.indexOf('=') + 1 );
-    
+document.addEventListener("DOMContentLoaded", () => {
+    const id = getUrlVars().id;
+    const api_key = "?api_key=4ed5aae0e5dd89775521a8ce7db46e80";
+    const base_url = "https://api.themoviedb.org/3/movie/";
     const api_url = base_url + id + api_key + '&language=es-ES';
     console.log(api_url );
     verDetalles(api_url );
+});
+     
+const getUrlVars = () => {
+    let vars = {};
+    window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+        vars[key] = value;
+
+    });
+    return vars;
+}
     
     function verDetalles(url) {
         const infopeli = document.getElementById('info');
+
         fetch(url).then(res => res.json())
         .then(function(data){
             const genres = data.genres;
             const annio = data.release_date.substr(0,4);
             console.log(genres);
+
             let movieDetailsString = `
             <div class=" fixed w-full h-full -z-1">`
-           
 
           infopeli.innerHTML += movieDetailsString;
     });
     }
-})
+
